fb226d3f46611347cd8c84dd69f38177f0c239b4
Ahmed <43687458+AhmedZaki918@users.noreply.github.com>
2019-05-08 15:35:13 +0200

    I changed the color of action bar and I added splash screen

---
+++ app/src/main/java/com/example/android/booksstore/SplashScreen.java
@@ -0,0 +1,34 @@
+package com.example.android.booksstore;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+
+
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+
+                try {
+                    sleep(3000);
+                    Intent i = new Intent(getApplicationContext(), CatalogActivity.class);
+                    startActivity(i);
+                    finish();
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+    }
+}
